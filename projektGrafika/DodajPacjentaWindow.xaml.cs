85683b1e074aad4f5b3dec9c68e4641efbc59e7a
Sebastian <litwiniuk.seba@gmail.com>
2020-12-27 16:48:57 +0100

    Edycja widoku głównego okna, dodanie widoku dodawania nowego pacjenta

---
+++ projektGrafika/DodajPacjentaWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace projektGrafika
+{
+    /// <summary>
+    /// Interaction logic for DodajPacjentaWindow.xaml
+    /// </summary>
+    public partial class DodajPacjentaWindow : Window
+    {
+        public DodajPacjentaWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ projektGrafika/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace projektGrafika
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string connectionString = "SERVER=localhost;DATABASE=projektgrafika;UID=root;PASSWORD=;";
+            MySqlConnection con = new MySqlConnection(connectionString);
+
+            void fillNameList()
+            {
+                try
+                {
+                    string query = "SELECT Name, Lastname FROM pacjent";
+                    MySqlCommand cmd = new MySqlCommand(query, con);
+                    con.Open();
+                    MySqlDataReader dr = cmd.ExecuteReader();
+
+                    while (dr.Read())
+                    {
+                        string name = dr.GetString(0);
+                        string lastName = dr.GetString(1);
+                        pacjentList.Items.Add(name + " " + lastName);
+                    }
+                    con.Close();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+            #region previous code
+            //DataTable dt = new DataTable();
+            //dt.Load(cmd.ExecuteReader());
+            //con.Close();
+            //pacjentList.Items.Add(dt);
+            #endregion
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            DodajPacjentaWindow add = new DodajPacjentaWindow();
+            add.Show();
+        }
+    }
+}
